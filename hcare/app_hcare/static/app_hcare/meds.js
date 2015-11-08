c673e064a507f9d09c626d5141f28505e4db5b03
Ben Nadler <bennadler@Bens-MacBook-Air.local>
2015-11-08 10:28:37 -0800

    add static dir

---
+++ hcare/app_hcare/static/app_hcare/meds.js
@@ -0,0 +1,7 @@
+	var available_meds = [
+      "Neisseria meningitidis serogroup",
+      "Immune Globulin (Human) 180 UNT/ML",
+      "Hepatitis B Surface Antigen Vaccine",
+      "Streptococcus pneumoniae serotype 14 capsular antigen diphtheria",
+      "L1 protein, Human papillomavirus type 11 Vaccine",
+      "Haemophilus influenzae b, capsular polysaccharide inactivated tetanus toxoid conjugate vaccine",]
